d44bd16aebe68d56419081d2083f80746da3c032
Elvin Huseynov <h.elvin@inbox.ru>
2020-01-23 11:54:23 +0400

    second comit

--- index.php
+++ index.php
@@ -29,39 +29,20 @@ if ($_GET['logout']) {
 
 if ($tg_user !== false) {
 
-  $first_name = htmlspecialchars($tg_user['first_name']);
-  $last_name = htmlspecialchars($tg_user['last_name']);
+    print_r($tg_user);
 
-  if (isset($tg_user['username'])) {
-    $username = htmlspecialchars($tg_user['username']);
-    $html = "<h1>Hello, <a href=\"https://t.me/{$username}\">{$first_name} {$last_name}</a>!</h1>";
-  } else {
-    $html = "<h1>Hello, {$first_name} {$last_name}!</h1>";
-  }
+} else { ?>
 
-  if (isset($tg_user['photo_url'])) {
-    $photo_url = htmlspecialchars($tg_user['photo_url']);
-    $html .= "<img src=\"{$photo_url}\">";
-  }
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Login Widget Example</title>
+</head>
+<body><center>
+    <h1>Hello, anonymous!</h1>
+    <script async src="https://telegram.org/js/telegram-widget.js?2" data-telegram-login="codex_az_bot" data-size="large" data-auth-url="index.php"></script>
+</center></body>
+</html>
 
-  $html .= "<p><a href=\"?logout=1\">Log out</a></p>";
-
-} else {
-
-  $html = <<<HTML
-<h1>Hello, anonymous!</h1>
-<script async src="https://telegram.org/js/telegram-widget.js?2" data-telegram-login="codex_az_bot" data-size="large" data-auth-url="index.php"></script>
-HTML;
-}
-
-
-  echo <<<HTML
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <title>Login Widget Example</title>
-      </head>
-      <body><center>{$html}</center></body>
-    </html>
-    HTML;
+<?php } ?>
